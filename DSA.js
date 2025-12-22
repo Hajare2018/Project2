140be250728a7e12f6c2fca3a10ddc8c097e15ec
Hajare2018 <103425553+Hajare2018@users.noreply.github.com>
2025-12-22 13:42:20 +0530

    Add string reversal and palindrome check functions

--- DSA.js
+++ DSA.js
@@ -32,3 +32,34 @@ function twoSum(nums, target) {
 
 // Example
 console.log(twoSum([2, 7, 11, 15], 9));
+
+
+// 1. Reverse a String
+
+let input = "hello brother";
+let output = ""
+function reverseString (str) {
+    for (let i = str.length -1; i >= 0; i--) {
+        output += str[i];
+    }
+    return output;
+}
+
+console.log(reverseString(input));
+
+// 2. Check for Palindrome
+
+let input = "madam";
+let output = ""
+function reverseString (str) {
+    for (let i = str.length -1; i >= 0; i--) {
+        output += str[i];
+    }
+    if (str === output) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(reverseString(input));
