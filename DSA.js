3ba0f12204671a265fd8e0692d795c99fa2318bf
Hajare2018 <103425553+Hajare2018@users.noreply.github.com>
2025-12-23 09:53:07 +0530

    Add factorial function and test in DSA.js

--- DSA.js
+++ DSA.js
@@ -80,4 +80,16 @@ function fibonacci () {
 }
 fibonacci()
 
+let num = 5;
 
+function main (num) {
+    let factorial = 1;
+    
+    for (let i = 1; i <=num; i++) {
+       factorial = factorial * i
+    }
+    
+    return factorial;
+}
+
+console.log(main(num));
